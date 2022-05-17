88e42ad6a608860d65cbc5ad3027a78246133c4a
rakim-up <ruraleproctor@gmail.com>
2022-05-18 01:11:48 +1200

    mole is showing but not in cells -.-

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,23 +2,37 @@
 // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log('Whack-a-Mole!')
+//the ability to store and get references from the HTML table.. like the tictactoe game I guess?
+//A function to randomly select which hole should get the mole
+//A way to show the mole in a chosen cell
 
-document.addEventListener('DOMContentLoaded', start)
+//references to the cells
+const holes = document.getElementsByTagName('TD') //get table array
 
-function start() {
-  bindEventListeners(document.getElementsByClassName('table')[0].children)
-  addMole(randomIntFromInterval(0, 24).toString())
+for (let i = 0; i < holes.length; i++) {
+  //loop thru
+  holes[i].onclick = WhackedMole
+  //we dont want cell to be clicked on we want the cell with mole image inside to be it
+  //bind on click to cells, maybe need it to just be on cell that mole pops up in
 }
 
-function bindEventListeners(table) {
-  for (var i = 0; i < table.length; i++) {
-    table[i].addEventListener('click', updateMoleMap)
-  }
-}
-function addMole(tableId) {
-  var img = document.createElement('img')
+//show the mole
+function showMoleRND(holes) {
+  //random cell selector
+  let randomIndex = Math.floor(Math.random() * 24)
+  let randomCell = holes[randomIndex]
+
+  let img = document.createElement('img') //creates image of mole from JS n adds to HTML
+  //now gotta append the img of the mole to the random cell selected
   img.src = './mole.PNG'
   img.id = 'moleImg'
-  document.getElementById(tableId).appendChild(img)
+
+  randomCell.appendChild(img) //appended
+  //problems..
+  //big ass mole appearing on the bottom of page not in hole
+  //why? maybe didnt append properly
+}
+function WhackedMole(e) {
+  let mole = e.target
+  showMole()
 }
